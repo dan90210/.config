8b5e4cf9a3be3980932d0a61e2600d9c787cceea
dan90210 <dan@debian.dan>
2023-11-26 10:25:51 -0600

    add other items

--- installs.sh
+++ installs.sh
@@ -1,1 +1,6 @@
-    sudo apt install i3 neovim git alacritty dmenu imagemagick scrot feh polybar rofi nodejs npm qbittorrent
+    sudo apt install i3 git alacritty dmenu imagemagick scrot feh polybar rofi nodejs npm qbittorrent ninja-build cmake gettext cmake unzip curl
+
+    # nvim install
+    git clone https://github.com/neovim/neovim
+    cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+    sudo make install
